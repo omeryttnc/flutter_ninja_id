3ae74fd34ed66f7fdbfe6c35629ef272604fbdd2
omeryttnc <67800186+omeryttnc@users.noreply.github.com>
2022-01-19 10:41:33 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: NinjaCard(),
+  ));
+
+}
+
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+        title: const Text('ninja'),
+      ),
+    );
+  }
+}
+
